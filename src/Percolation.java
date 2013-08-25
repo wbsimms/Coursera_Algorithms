a26f0d5c1ea4298c974e7e4bbd37ec8155301eb4
William Barrret Simms <wbsimms@gmail.com>
2013-08-25 16:38:06 -0400

    first pass

--- src/Percolation.java
+++ src/Percolation.java
@@ -10,47 +10,53 @@ public class Percolation {
         width = height = N;
         numberOfSites = N^2;
         quickUnion = new WeightedQuickUnionUF(numberOfSites+2);
+        sites = new int[numberOfSites];
+        for (int i = 0; i<= sites.length; i++)
+        {
+            sites[i] = 0; // all sites are closed
+        }
+        for(int i = 0; i<=width; i++)
+        {
+            quickUnion.union(0, i);
+            quickUnion.union(sites.length, sites.length-i);
+        }
     }
     
     public void open(int i, int j) {
         if (i > height || i < 1 || j > width || j < 1) throw new IndexOutOfBoundsException();
+
+        
         int cellNum = positionNumber(i,j); // get the position number;
+        if (sites[cellNum] == 1) return;
+        sites[cellNum] = 1;
         
         // this connects the cells above and below
-        if (i < height) quickUnion.union(cellNum, cellNum+width);
-        if (i > 1)  quickUnion.union(cellNum, cellNum-width);
+        if (i < height && isOpen(i+1,j)) quickUnion.union(cellNum, cellNum+width); // down
+        if (i > 1 && isOpen(i-1,j))  quickUnion.union(cellNum, cellNum-width); // up
 
         // this connects the cells left and right
-        if (j > 1) quickUnion.union(cellNum, cellNum-1);
-        if (j < width) quickUnion.union(cellNum, cellNum+1);
+        if (j > 1 && isOpen(i,j-1)) quickUnion.union(cellNum, cellNum-1);
+        if (j < width && isOpen(i,j+1)) quickUnion.union(cellNum, cellNum+1);
     }
     
     private int positionNumber(int i, int j)
     {
         return ((i-1)*width)+j; // get the position number;
     }
-    
+
     public boolean isOpen(int i, int j) 
     {
         int cellNum = positionNumber(i,j); // get the position number;
-        // this connects the cells above and below
-        if (i < height && quickUnion.connected(cellNum, cellNum+width)) {return true;}
-        if (i > 1 && quickUnion.connected(cellNum, cellNum-width)) {return true;}
-
-        // this connects the cells left and right
-        if (j > 1 && quickUnion.connected(cellNum, cellNum-1)) {return true;}
-        if (j < width && quickUnion.connected(cellNum, cellNum+1)) {return true;}
-        return false;
+        return sites[cellNum] == 1;
     }
     
     public boolean isFull(int i, int j) {
-    
-        return false;
+        int toTest = positionNumber(i,j);
+        return quickUnion.connected(0, toTest);
     }
     
     public boolean percolates() {
-        
-        return false;
+        return quickUnion.connected(0, sites.length);
     }
     
 }
